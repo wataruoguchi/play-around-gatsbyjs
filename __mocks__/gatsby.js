47097c556e2c1ee2b955b0b157fb54a0a8300915
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-08-25 17:46:08 -0700

    Improve lint for jest

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -1,25 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// TODO ^^ 'Convert ES6' didn't work
 const React = require("react")
 const gatsby = jest.requireActual("gatsby")
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(
-    // these props are invalid for an `a` tag
-    ({
-      activeClassName,
-      activeStyle,
-      getProps,
-      innerRef,
-      partiallyActive,
-      ref,
-      replace,
-      to,
-      ...rest
-    }) =>
-      React.createElement("a", {
-        ...rest,
-        href: to,
-      })
+  Link: jest.fn().mockImplementation(({ to, ...rest }) =>
+    React.createElement("a", {
+      ...rest,
+      href: to,
+    })
   ),
   StaticQuery: jest.fn(),
   useStaticQuery: jest.fn(),
