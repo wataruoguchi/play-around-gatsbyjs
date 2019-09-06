8f8f96d5d11f0b423621489b7716d391f57bb33a
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2019-09-05 23:19:02 -0700

    Move the test dir

--- __tests__/header.js
+++ __tests__/header.js
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import Header from "../header"
+import Header from "../src/components/header"
 describe("Header", () => {
   it("renders correctly", () => {
     const tree = renderer
